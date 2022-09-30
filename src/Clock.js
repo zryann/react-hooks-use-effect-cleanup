b0dce5da4271430fefb345aed4dee67169a38297
Zryan Muhammed Jaafer <zryan.muhammed.jaafer@gmail.com>
2022-09-30 17:33:16 +0300

    Done!

--- src/Clock.js
+++ src/Clock.js
@@ -4,9 +4,14 @@ function Clock() {
   const [time, setTime] = useState(new Date());
 
   useEffect(() => {
-    setInterval(() => {
+    const timerID = setInterval(() => {
       setTime(new Date());
     }, 1000);
+
+    // returning a cleanup function
+    return function cleanup() {
+      clearInterval(timerID);
+    };
   }, []);
 
   return <div>{time.toString()}</div>;
